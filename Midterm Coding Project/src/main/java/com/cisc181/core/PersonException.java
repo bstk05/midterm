355d31816f909337572b5fac5a48c27a66f4de79
12965 <12965@DESKTOP-FLIBO64>
2017-03-18 19:44:19 -0400

    3

--- Midterm Coding Project/src/main/java/com/cisc181/core/PersonException.java
+++ Midterm Coding Project/src/main/java/com/cisc181/core/PersonException.java
@@ -5,24 +5,13 @@ import java.util.Date;
 public class PersonException extends Exception {
 	
 	private Person Person;
-	private String invalidPN;
-	private Date invalidDOB;
 	
 	public PersonException(Person Person){
 	
 		this.Person = Person;
 	}
 
-	public PersonException(String invalidPN){
-		
-		this.invalidPN = invalidPN;
-	}
-	
-	public PersonException(Date invalidDOB){
-		
-		this.invalidDOB = invalidDOB;
-	}
-	
+
 	
 	
 	
